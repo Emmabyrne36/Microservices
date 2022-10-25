b1faa76dfbe56246e85256e138b9102af4220b68
Emmabyrne36 <emmabyrne36@gmail.com>
2022-10-25 10:09:12 +0100

    Set up repository and seeded data

---
+++ PlatformService/Data/SeedData.cs
@@ -0,0 +1,33 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public static class SeedData
+    {
+        public static void SeedPopulation(IApplicationBuilder builder)
+        {
+            using (var serviceScope = builder.ApplicationServices.CreateScope())
+            {
+                Seed(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void Seed(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding Data...");
+                context.Platforms.AddRange(
+                    new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
+                );
+
+                context.SaveChanges();
+                return;
+            }
+
+            Console.WriteLine("--> We already have data.");
+        }
+    }
+}
